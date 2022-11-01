af6623925f119d9cabfbd366e6fbfd1fc33ed595
Spykerwolf <willsmal.nz@gmail.com>
2022-11-02 00:14:48 +1300

    Hotkey working

--- potplayerAuto.py
+++ potplayerAuto.py
@@ -5,20 +5,34 @@ import os
 import shutil
 
 while True:
-    if keyboard.is_pressed('m'):
-        print("Script activated")
+    if keyboard.is_pressed('F11'):
+        pyperclip.copy('')
         pyautogui.hotkey('ctrl','f1')
         pyautogui.press(['tab', 'tab'])
         pyautogui.press('right')
         pyautogui.press('space')
         pyautogui.press(['tab', 'tab'])
         pyautogui.press(['down', 'down'])
-        source = pyautogui.hotkey('ctrl','c')
+        
+        #Copies source file name
+        pyautogui.hotkey('ctrl','c') 
         pyautogui.press(['tab', 'tab'])
         pyautogui.press('space')
         pyautogui.press(['f6', 'f6'])
         pyautogui.press('delete')
-        fileNameRaw = pyperclip.paste().partition(":")
-        sourceFileName = fileNameRaw[2]
-        print(sourceFileName)
-        break
+        
+        #Trim file name
+        fileNameRaw = pyperclip.paste().split(":", 1)
+        sourceLong = fileNameRaw[1].strip()
+        print(sourceLong)
+        
+        
+        sourceShort = sourceLong.split("\\")
+        print(sourceShort)
+        source = sourceShort[2]
+        print(source)
+        
+        destination = r"F:\#######" + "\\" + source
+        print(destination)
+        shutil.move(sourceLong, destination)
+        pyautogui.hotkey('enter')
